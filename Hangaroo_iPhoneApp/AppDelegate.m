a732128edba0aa533ba6251d3a6a532f47463c00
Hemlata <hemlata@ranosys.com>
2016-01-13 18:43:26 +0530

    Post listing web service implemented.

--- Hangaroo_iPhoneApp/AppDelegate.m
+++ Hangaroo_iPhoneApp/AppDelegate.m
@@ -60,16 +60,16 @@ id<GAITracker> tracker;
     // Override point for customization after application launch.
    
     // Optional: automatically send uncaught exceptions to Google Analytics.
-//    [GAI sharedInstance].trackUncaughtExceptions = YES;
-//    
-//    // Optional: set Google Analytics dispatch interval to e.g. 20 seconds.
-//    [GAI sharedInstance].dispatchInterval = 5;
-//    
-//    // Optional: set Logger to VERBOSE for debug information.
-//    [[[GAI sharedInstance] logger] setLogLevel:kGAILogLevelVerbose];
-//    
-//    // Initialize tracker. Replace with your tracking ID.
-//    tracker = [[GAI sharedInstance] trackerWithTrackingId:@"UA-72052944-1"];
+    [GAI sharedInstance].trackUncaughtExceptions = YES;
+    
+    // Optional: set Google Analytics dispatch interval to e.g. 20 seconds.
+    [GAI sharedInstance].dispatchInterval = 5;
+    
+    // Optional: set Logger to VERBOSE for debug information.
+    [[[GAI sharedInstance] logger] setLogLevel:kGAILogLevelVerbose];
+    
+    // Initialize tracker. Replace with your tracking ID.
+    tracker = [[GAI sharedInstance] trackerWithTrackingId:@"UA-72052944-1"];
    
     
     // [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"header.png"] forBarMetrics:UIBarMetricsDefault];
