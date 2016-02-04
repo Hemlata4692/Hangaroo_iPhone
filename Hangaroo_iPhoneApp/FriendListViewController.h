e1e93defc057e404e5a71797eb3248d44b4ea0ea
Hemlata <hemlata@ranosys.com>
2016-02-04 15:43:39 +0530

    Friend list screen designing started.

---
+++ Hangaroo_iPhoneApp/FriendListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendListViewController.h
+//  Hangaroo_iPhoneApp
+//
+//  Created by Hema on 04/02/16.
+//  Copyright © 2016 Ranosys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendListViewController : UIViewController
+
+@end
